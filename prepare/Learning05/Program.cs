6f6aaa52dd2c214b52f8f7429acf3c491918e687
hal22036 <jeni.lee.hales@gmail.com>
2023-03-15 08:19:48 -0700

    learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("blue", 4);
+        Console.WriteLine($"{square.GetColor()} {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("red", 3, 6);
+        Console.WriteLine($"{rectangle.GetColor()} {rectangle.GetArea()}");
+
+        Circle circle = new Circle("white", 4);
+        Console.WriteLine($"{circle.GetColor()} {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea()}.");
+        }
     }
 }
